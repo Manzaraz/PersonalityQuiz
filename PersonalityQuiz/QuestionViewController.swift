efb0612e232b746c2945e8fe090fd28fc8976e82
Christian Manzaraz <magnusmanz@gmail.com>
2022-12-07 00:13:44 -0300

    Display Questions and Answers

--- PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/QuestionViewController.swift
@@ -8,6 +8,40 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
+        
+    
+    var questions: [Question] = [
+        Question(
+            text: "¿Qué comida te gusta más?",
+            type: .single,
+            answers: [
+                Answer(text: "Bife", type: .dog),
+                Answer(text: "Salmón", type: .cat),
+                Answer(text: "Zanahorias", type: .rabbit),
+                Answer(text: "Lechuga", type: .turtle)
+            ]
+        ),
+        Question(
+            text: "¿Qué actividades te gustan?",
+            type: .multiple,
+                 answers: [
+                    Answer(text: "Nadar", type: .turtle),
+                    Answer(text: "Dormir", type: .cat),
+                    Answer(text: "Acurrucarte", type: .rabbit),
+                    Answer(text: "Comer", type: .dog)
+                 ]
+                ),
+        Question(
+            text: "¿Qué tanto te gustan los viajes en auto?",
+            type: .ranged,
+            answers: [
+                Answer(text: "No me gustan", type: .cat),
+                Answer(text: "Me pongo un poco nervioso", type: .rabbit),
+                Answer(text: "¿De qué viaje estás hablando?", type: .turtle),
+                Answer(text: "Adoro ir de paseo", type: .dog)
+            ]
+        )
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
